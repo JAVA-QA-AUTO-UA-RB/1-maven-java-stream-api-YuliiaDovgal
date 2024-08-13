2b0442844066204f08ee8d9451692d2b65c78a98
YuliiaDovgal <dovgaljulka@gmail.com>
2024-08-13 14:35:13 +0300

    Homework1_Maven

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,14 +11,12 @@ public class Main {
     public static final String INPUT_FILENAME = "users.json";
 
     public static void main(String[] args) throws IOException {
-        // Не змінюйте чотири рядки нижче
+
         String inputFileName = INPUT_FILENAME;
         String jsonFilePath = ResourceLoader.getResourcePath(inputFileName);
         List<User> importedUsers = UserLoader.loadUsersFromJson(jsonFilePath);
         List<User> filteredAndSortedUsers = UserProcessor.filterAndSortUsers(importedUsers);
 
-        //Переконайтеся, що в результаті виконання програми ДІЙСНО надрукується
-        //список юзерів, які відфільтровані і відсортовані за вказаними критеріями
         filteredAndSortedUsers.forEach(System.out::println);
 
     }
